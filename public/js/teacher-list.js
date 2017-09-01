91fcbd5ce234f93cd1446f2030baa99da79b91fa
Zhangsan <123456@qq.com>
2017-09-01 22:48:16 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -11,9 +11,7 @@ define(['jquery','template','bootstrap'],function($,template){
 
 			// 绑定预览单击事件(如果写在下面，那么按钮还没有生成，没法绑定)
 			$('.preview').click(function(){
-				//获取当前记录的ID
-				var td=$(this).closest('td');//离a标签最近的父元素
-				var tcId=td.attr('data-tcId');
+				
 				//根据ID查询数据
 				$.ajax({
 					type : 'get',
@@ -29,6 +27,34 @@ define(['jquery','template','bootstrap'],function($,template){
 					}
 				})
 			});
+
+			//启用注销功能
+			$('.eod').click(function(){
+			//获取当前记录的ID
+				var td=$(this).closest('td');//离a标签最近的父元素
+				var tcId=td.attr('data-tcId');
+				var tcStatus=td.attr('data-status');
+				var that=this;
+				//调用接口
+				$.ajax({
+					type : 'post',
+					url : '/api/teacher/handle',
+					data : {tc_id : tcId,tc_status : tcStatus},
+					dataType : 'json',
+					success:function(data){
+						if(data.code==200){
+							//修改当前状态
+							td.attr('data-status',data.result.tc_status);
+							//修改文字信息
+							if(data.result.tc_status==0){
+								$(that).html('注 销');
+							}else{
+								$(that).html('启 用');
+							}
+						}
+					}
+				})
+			})
 		}
 	});
 

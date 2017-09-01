126411ec0400de73afe86968a971c56b2e55a83b
Zhangsan <123456@qq.com>
2017-09-01 22:18:48 +0800

    讲师查看功能

--- public/js/config.js
+++ public/js/config.js
@@ -5,8 +5,14 @@ require.config({
     jquery : 'jquery/jquery',
     cookie : 'jquery-cookie/jquery.cookie',
     template : 'artTemplate/template-web',
+    bootstrap : 'bootstrap/js/bootstrap.min',
     common : '../js/common',
     login : '../js/login',
     teacherlist : '../js/teacher-list'
+  },
+  shim : {//垫片，将非标准模块转换为标准模块
+  	bootstrap : {
+  		deps : ['jquery']
+  	}
   }
 });

64b560818a78b39709582bf107165accd7c702d0
Zhangsan <123456@qq.com>
2017-09-03 21:28:30 +0800

    添加提起选择插件

--- public/js/config.js
+++ public/js/config.js
@@ -6,6 +6,8 @@ require.config({
     cookie : 'jquery-cookie/jquery.cookie',
     template : 'artTemplate/template-web',
     bootstrap : 'bootstrap/js/bootstrap.min',
+    datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
+    language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',//非标准模块，依赖jquery和datepicker
     common : '../js/common',
     login : '../js/login',
     index : '../js/index',
@@ -16,6 +18,10 @@ require.config({
   shim : {//垫片，将非标准模块转换为标准模块
   	bootstrap : {
   		deps : ['jquery']
-  	}
+  	},
+    language : {
+      deps :['jquery','datepicker']
+    }
+
   }
 });
